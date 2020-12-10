fca6fa2530e7883cecdc1f8df90a06ae676c8189
Bartlomiej Krolikowski <bartlomiej.krolikowski@yahoo.com>
2020-12-10 11:53:18 +0100

    Begun implementation of the board

---
+++ src/main/java/Project/common/board/Field.java
@@ -0,0 +1,12 @@
+package Project.common.board;
+
+public class Field {
+
+	private Field[] neighbours = new Field[6];
+	private int fieldId;
+
+	public void setNeighbour(Field neighbour) {
+		// TODO
+	}
+
+}
